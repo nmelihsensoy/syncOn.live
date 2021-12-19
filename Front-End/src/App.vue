f890d559637e1949917e9fd1b161e383277f8733
nmelihsensoy <nmelihsensoy@gmail.com>
2021-12-19 08:12:28 +0300

    Fixed plyr css issue with cdn

--- Front-End/src/App.vue
+++ Front-End/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
       <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+      <link rel="stylesheet" href="https://cdn.plyr.io/3.6.12/plyr.css" />
       <div id="wrapper">
         <router-view/>
       </div>
@@ -13,7 +14,7 @@
 import Footer from './components/Footer'
 import Vue from 'vue'
 import VuePlyr from 'vue-plyr'
-import 'vue-plyr/dist/vue-plyr.css'
+//import 'vue-plyr/dist/vue-plyr.css'
 
 import 'bulma/css/bulma.css'
 
